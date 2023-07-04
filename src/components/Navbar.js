034abea31ff2b20bab81013ea051013a64caecc1
Braian Vargas <braianvargas1616@gmail.com>
2023-07-04 00:24:03 -0300

    [add] style news on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,17 +7,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
      var navbar = document.getElementById("navbar");
-     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 1) {
           navbar.classList.add('scrolled');
      } else {
           navbar.classList.remove('scrolled');
      }
 }
 
-function scrollTop() {
-     document.body.scrollTop = 0;
-     document.documentElement.scrollTop = 0;
-}
 
 export default class Navbar extends Component {
   render() {
@@ -25,22 +21,27 @@ export default class Navbar extends Component {
                <div>
                     <nav id='navbar'  className="navbar navbar-expand-lg ">
                          <div className="container-fluid">
-                              <a className="logo navbar-brand" href="/">
-                                   <img  alt='logo-promtel' src={logo}/>
-                              </a>
                               <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                    <span className="navbar-toggler-icon"></span>
                               </button>
                               <div className="collapse navbar-collapse navlinks" id="navbarSupportedContent">
                                    <ul className="navbar-nav mb-2 mb-lg-0 ">
                                         <li className="nav-item">
-                                             <a className="links active" aria-current="page" href={'/'}>Inicio</a>
+                                             <a className="links" aria-current="page" href={'/'}>Inicio</a>
                                         </li>
                                         <li className="nav-item">
-                                             <a className="links active" aria-current="page" href={'/about'}>Quienes Somos</a>
+                                             <a className="links" aria-current="page" href={'#'}>Quienes somos</a>
+                                        </li>
+                                        <li className='nav-item'>
+                                             <a className="logo navbar-brand" href="/">
+                                                  <img  alt='logo-promtel' src={logo}/>
+                                             </a>
                                         </li>
                                         <li className="nav-item">
-                                             <a className="links active" aria-current="page" href={'/contacts'}>Contacto</a>
+                                             <a className="links" aria-current="page" href={'/about'}>Nuestro trabajo</a>
+                                        </li>
+                                        <li className="nav-item">
+                                             <a className="links" aria-current="page" href={'/contacts'}>Contacto</a>
                                         </li>
                                    </ul>
                               </div>
